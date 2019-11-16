86437dbcc185b97e6b5e83ee543307e92dcb26bc
zhaodandan427 <zhaodd@gocom.ai>
2019-11-16 17:14:45 +0800

    feat:提价代码

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css'; 
+import 'antd/dist/antd.css';
+import 'antd-mobile/dist/antd-mobile.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/createAlbum/createAlbum.js
+++ src/pages/createAlbum/createAlbum.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './createAlbum.scss';
-
+import { ImagePicker, WingBlank, SegmentedControl } from 'antd-mobile';
 class CreateAlbum extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      files: [
+        // {
+        //   url: 'https://zos.alipayobjects.com/rmsportal/PZUUCKTRIHWiZSY.jpeg',
+        //   id: '2121',
+        // }, {
+        //   url: 'https://zos.alipayobjects.com/rmsportal/hqQWgTXdrlmVVYi.jpeg',
+        //   id: '2122',
+        // }
+      ],
+      multiple: false,
+
+    }
+
   }
+  onChange = (files, type, index) => {
+    console.log(files, type, index);
+    this.setState({
+      files,
+    });
+  }
+  
   render() {
+    const files=this.state.files;
     return (
       <div className={'createAlbum-wrap'}>
         <header>
@@ -15,6 +36,19 @@ class CreateAlbum extends React.Component {
           创建相册
         <Link to={'/'}><span>完成</span></Link>
         </header>
+        <div className={'createAlbum-content'}>
+          <div className={'album-description'}>请输入相册介绍</div>
+          <WingBlank>
+            
+            <ImagePicker
+              files={files}
+              onChange={this.onChange}
+              onImageClick={(index, fs) => console.log(index, fs)}
+              selectable={files.length < 10}
+              multiple={this.state.multiple}
+            />
+          </WingBlank>
+        </div>
       </div>
     )
   }
